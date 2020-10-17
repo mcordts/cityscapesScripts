Bugfix/bev depth calculation (#135)
* use correct axis for BEV calculation
* bugfix: results folder handling

Co-authored-by: Nils Gaehlert <nils.gaehlert@daimler.com>
Co-authored-by: Marius Cordts <marius.cordts@daimler.com>

cityscapesscripts/evaluation/evalObjectDetection3d.py
@@ -579,11 +579,11 @@ class Box3dEvaluator:
         pred_boxes = np.asarray([x.center for x in pred_boxes])
 
         gt_dists = np.sqrt(gt_boxes[..., 0]**2 +
-                           gt_boxes[..., 2]**2).astype(int)
+                           gt_boxes[..., 1]**2).astype(int)
 
         center_dists = gt_boxes - pred_boxes
         center_dists = np.sqrt(center_dists[..., 0]**2 +
-                               center_dists[..., 2]**2)
+                               center_dists[..., 1]**2)
 
         for gt_dist, center_dist in zip(gt_dists, center_dists):
             if gt_dist >= self.eval_params.max_depth:
@@ -1183,11 +1183,10 @@ def main():
                                           default=predictionFolder,
                                           type=str)
 
-    resultFolder = ""
     parser.add_argument("--results-folder",
                         dest="resultsFolder",
                         help="File to store evaluation results. Default: prediction folder",
-                        default=resultFolder,
+                        default="",
                         type=str)
 
     # setup evaluation parameters
@@ -1245,11 +1244,12 @@ def main():
         logger.error(msg)
         raise argparse.ArgumentError(pred_folder_arg, msg)
 
-    if resultFolder == "":
-        resultFolder = args.predictionFolder
+    resultsFolder = args.resultsFolder
+    if not resultsFolder:
+        resultsFolder = args.predictionFolder
     # keep python 2 compatibility
-    if not os.path.exists(resultFolder):
-        os.makedirs(resultFolder)
+    if not os.path.exists(resultsFolder):
+        os.makedirs(resultsFolder)
 
     # setup the evaluation parameters
     eval_params = EvaluationParameters(
@@ -1264,7 +1264,7 @@ def main():
     evaluate3dObjectDetection(
         args.gtFolder,
         args.predictionFolder,
-        resultFolder,
+        resultsFolder,
         eval_params,
         plot=args.plot_results
     )

cityscapesscripts/helpers/annotation.py
@@ -293,7 +293,8 @@ class CsBbox3d(CsObject):
 
     @property
     def depth(self):
-        return np.sqrt(self.center[0]**2 + self.center[2]**2).astype(int)
+        # returns the BEV depth
+        return np.sqrt(self.center[0]**2 + self.center[1]**2).astype(int)
 
 
 class CsIgnore2d(CsObject):